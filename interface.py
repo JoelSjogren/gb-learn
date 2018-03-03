69ff9887dd99ddbc00df347a2f9fe20f0b942256
Joel Sjögren <joelsjogren.wii@gmail.com>
2018-03-03 19:08:48 +0100

    add pygame interaction

--- interface.py
+++ interface.py
@@ -8,6 +8,7 @@ sys.path.append(mgba_python_path)
 # ENDOFUGLYHACK
 
 import mgba, mgba.core, mgba.image, mgba.gb
+import pygame
 
 class Gameboy():
     def __init__(self, rom_path):
@@ -30,6 +31,7 @@ class Gameboy():
     # run a few frames of the loaded game and store a screenshot
     def demo(self):
         image = mgba.image.Image(*self._core.desiredVideoDimensions())
+        self._image = image
         self._core.setVideoBuffer(image)
         self._core.reset()  # important step
         
@@ -68,3 +70,45 @@ KEY_DOWN = mgba.gb.GB.KEY_DOWN
 KEY_RIGHT = mgba.gb.GB.KEY_RIGHT
 KEY_SELECT = mgba.gb.GB.KEY_SELECT
 KEY_START = mgba.gb.GB.KEY_START
+
+crystal = "../pokemon-crystal.gbc"
+
+def play(rom_path):
+    from PIL import Image
+
+    screen = pygame.display.set_mode((160,144))
+    
+    gb = Gameboy(rom_path)
+    image = mgba.image.Image(*gb._core.desiredVideoDimensions())
+    gb._core.setVideoBuffer(image)
+    gb._core.reset()  # important step
+
+    clock = pygame.time.Clock()
+    while True:
+        pygame.event.pump()
+        keys = pygame.key.get_pressed()
+        gb.setKeys(*convertKeys(keys))
+        for _ in range(50 if keys[pygame.K_SPACE] else 1):
+            gb._core.runFrame()
+            
+        pil = Image.frombytes("RGBX", (160, 144),
+                              mgba._pylib.ffi.buffer(image.buffer),
+                              "raw", "RGBX", image.stride * 4)
+        pyg = pygame.image.fromstring(pil.tobytes(), pil.size, pil.mode)
+
+        screen.blit(pyg, pyg.get_rect())
+        pygame.display.flip()
+        clock.tick(60)
+
+def convertKeys(keys):
+    f = ((KEY_A, pygame.K_x),
+         (KEY_B, pygame.K_z),
+         (KEY_UP, pygame.K_UP),
+         (KEY_LEFT, pygame.K_LEFT),
+         (KEY_DOWN, pygame.K_DOWN),
+         (KEY_RIGHT, pygame.K_RIGHT),
+         (KEY_SELECT, pygame.K_BACKSPACE),
+         (KEY_START, pygame.K_RETURN))
+    return [i for i, j in f if keys[j]]
+
+#play(crystal)
